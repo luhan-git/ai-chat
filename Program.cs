bb614a9b28170a4f73c52ba2a8036fcf51c36716
omar <n00209455@upn.pe>
2024-12-13 14:30:00 -0500

    init example code

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenAI;
+using Microsoft.Extensions.AI;
+
+var config=new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+string model=config["Modelname"]??"openai";
+string key=config["OpenAIkey"]??"key";
+
+IChatClient chatClient =
+    new OpenAIClient(key).AsChatClient(model);
+
+List<ChatMessage> chatHistory =
+    [
+        new ChatMessage(ChatRole.System, """
+            Eres un asistente malhablado
+        """)
+    ];
+
+    while (true)
+{
+    // Get user prompt and add to chat history
+    Console.WriteLine("Your prompt:");
+    var userPrompt = Console.ReadLine();
+    chatHistory.Add(new ChatMessage(ChatRole.User, userPrompt));
+
+    // Stream the AI response and add to chat history
+    Console.WriteLine("AI Response:");
+    var response = "";
+    await foreach (var item in
+        chatClient.CompleteStreamingAsync(chatHistory))
+    {
+        Console.Write(item.Text);
+        response += item.Text;
+    }
+    chatHistory.Add(new ChatMessage(ChatRole.Assistant, response));
+    Console.WriteLine();
+}
